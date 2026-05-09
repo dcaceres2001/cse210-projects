1c0cc73142ffea41bc8a0990871dd4b605a98fcf
douglasc2001 <douglasc_2001@hotmail.com>
2026-05-09 13:34:23 -0600

    Execise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Input your grade");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string gradeLetter = "";
+
+        if (gradeInt >= 90)
+        {
+             gradeLetter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+             gradeLetter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+             gradeLetter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+             gradeLetter = "D";
+        }
+        else
+        {
+             gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class.");
+        }
+
+
     }
 }
