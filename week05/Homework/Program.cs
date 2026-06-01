d37129c9b7fc32164ac478cf58fbb0a8012b0d71
douglasc2001 <douglasc_2001@hotmail.com>
2026-06-01 17:39:43 -0600

    inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            Assignment assignment1 = new Assignment("John Doe", "Math");
+            Console.WriteLine(assignment1.GetSummary());
+
+            MathAssignment mathAssignment1 = new MathAssignment("Jane Smith", "Math", "Section 5.2", "Problems 1-10");
+            Console.WriteLine(mathAssignment1.GetSummary());
+            Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+            WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writingAssignment1.GetSummary());
+            Console.WriteLine(writingAssignment1.GetWritingInformation());
+        }
     }
+
 }
